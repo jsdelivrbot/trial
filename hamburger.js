ff020371b931842c317938e1c385c435f42e714b
marmousett <30293049+marmousett@users.noreply.github.com>
2017-08-07 13:07:06 -0700

    Delete hamburger.js

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-var menus = document.getElementsByClassName('hamburger-menu');
-
-[].forEach.call(menus, function (m) {
-    m.addEventListener('click', function () {
-        m.classList.toggle('open');
-    });
-});
